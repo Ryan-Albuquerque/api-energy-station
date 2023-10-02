f20319f0474a969010ed2581373887412f6e7e33
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2023-10-02 03:57:34 -0400

    add user main

--- src/modules/users/main.ts
+++ src/modules/users/main.ts
@@ -1,2 +1,14 @@
-// DI
-export const userModule = () => {};
+import { UserModel } from "./model/user.model";
+import { UserRepository } from "./repository/user.repository";
+import { UserResolver } from "./resolvers/user.resolver";
+import { UserService } from "./services/user.service";
+
+const main = () => {
+  const repository = new UserRepository(UserModel);
+  const userService = new UserService(repository);
+  const { Query, Mutation } = new UserResolver(userService);
+
+  return { Query, Mutation, userService };
+};
+
+export const userModule = main();
